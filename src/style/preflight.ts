41f3f64553de1cc418d61245e6894810afc87591
nousantx <nousantx@gmail.com>
2025-03-26 11:33:35 +0700

    feat: update preflight

--- src/style/preflight.ts
+++ src/style/preflight.ts
@@ -17,14 +17,13 @@ export const resetter: ApplyStyleObject = {
       [fontFeatureSettings,fontVariationSettings]-normal
       [webkitTapHighlightColor]-transparent
     `,
-  body: 'leading-inherit',
   hr: 'h-0 text-inherit border-t-1',
   'abbr:where([title])': '[-webkit-text-decoration,text-decoration]-[underline_dotted]',
   'h1, h2, h3, h4, h5, h6': 'text-(size:inherit) leading-inherit',
   a: '[color,-webkit-text-decoration,text-decoration]-inherit',
   'b, strong': '[fontWeight]-bolder',
   'code, kbd, samp, pre':
-    'font(family:{default-font-mono}) [fontFeatureSettings]-normal [fontVariationSettings]-normal text-1em',
+    'font-(family:{default-font-mono}) [fontFeatureSettings]-normal [fontVariationSettings]-normal text-1em',
   small: 'text-80%',
   'sub, sup': 'text-75% leading-0 relative align-baseline',
   sub: 'bottom--1',
@@ -35,17 +34,18 @@ export const resetter: ApplyStyleObject = {
   summary: 'list-item',
   'ol, ul, menu': '[list-style]-none',
   'img, svg, video, canvas, audio, iframe, embed, object': 'block align-middle',
-  'img, video': 'w-max-100% h-auto',
+  'img, video': 'max-w-100% h-auto',
   'button, input, select, optgroup, textarea, ::file-selector-button':
     '[font,color,letterSpacing,fontFeatureSettings,fontVariationSettings]-inherit bg-transparent radius-0 opacity-1',
 
   ':where(select:is([multiple], [size])) optgroup': '[fontWeight]-bolder',
   ':where(select:is([multiple], [size])) optgroup option': '[paddingInlineStart]-20px',
   '::file-selector-button': '[marginInlineEnd]-4px',
-  '::placeholder': 'opacity1 [color]-[color-mix(in_oklab,_currentColor_50%,_transparent)]',
+  '::placeholder':
+    'opacity-1 [@supports_(not_(-webkit-appearance:_-apple-pay-button))_or_(contain-intrinsic-size:1px)]:[color]-[color-mix(in_oklab,_currentColor_50%,_transparent)]',
   textarea: 'resize-vertical',
   '::-webkit-search-decoration': '[webkitAppearance]-none',
-  '::-webkit-date-and-time-value': 'h-min-1lh [textAlign]-inherit',
+  '::-webkit-date-and-time-value': 'min-h-1lh [textAlign]-inherit',
   '::-webkit-datetime-edit': 'inline-flex',
   '::-webkit-datetime-edit-fields-wrapper': 'p-0',
   '::-webkit-datetime-edit, ::-webkit-datetime-edit-year-field, ::-webkit-datetime-edit-month-field, ::-webkit-datetime-edit-day-field, ::-webkit-datetime-edit-hour-field, ::-webkit-datetime-edit-minute-field, ::-webkit-datetime-edit-second-field, ::-webkit-datetime-edit-millisecond-field, ::-webkit-datetime-edit-meridiem-field':
