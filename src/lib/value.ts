adce1adadda6b9a825d3f08191134f4f36a6e5cc
nousantx <nousantx@gmail.com>
2025-04-05 11:33:09 +0700

    feat: add border-radius value group

--- src/lib/value.ts
+++ src/lib/value.ts
@@ -52,5 +52,15 @@ export const values: Values = {
     xl: '24px',
     '2xl': '40px',
     '3xl': '64px'
+  },
+  'anc-border-radius-value': {
+    xs: '0.125rem',
+    sm: '0.25rem',
+    md: '0.375rem',
+    lg: '0.5rem',
+    xl: '0.75rem',
+    '2xl': '1rem',
+    '3xl': '1.5rem',
+    '4xl': '2rem'
   }
 }
